394a2daca849e47261d0d13f06afd60ea7dd722b
a-golosnyak <andrey_g.pt@mail.ru>
2020-11-12 22:25:50 +0200

    Frontend built.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 window._ = require('lodash');
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import axios from 'axios';
 import router from './router.js';
 
@@ -11,7 +11,7 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+    // require('bootstrap');
 } catch (e) {}
 
 window.axios = axios;
---
+++ frontend/src/router.js
@@ -0,0 +1,68 @@
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// import PostIndex from './components/router/post/PostIndex.vue';
+// import PostShow from './components/router/post/PostShow.vue';
+// import PostCreateUpdate from './components/router/post/PostCreateUpdate.vue';
+// import LoginPage from './components/router/page/LoginPage.vue';
+// import RegisterPage from './components/router/page/RegisterPage.vue';
+// import UserIndex from './components/router/user/UserShow.vue';
+// import UserUpdate from './components/router/user/UserUpdate.vue';
+//
+ let router = new VueRouter({
+//     mode: 'history',
+//     routes: [
+//         {
+//             path: '/',
+//             component: PostIndex,
+//             name: 'home'
+//         },
+//
+//         {
+//             path: '/user/:id',
+//             component: UserIndex,
+//             name: 'user.index',
+//             props: true,
+//         },
+//         {
+//             path: '/user/:id/edit',
+//             component: UserUpdate,
+//             name: 'user.edit',
+//             props: true,
+//         },
+//
+//         {
+//             path: '/post/create',
+//             component: PostCreateUpdate,
+//             name: 'post.create',
+//             props: true,
+//         },
+//         {
+//             path: '/post/:id/edit',
+//             component: PostCreateUpdate,
+//             name: 'post.edit',
+//             props: true,
+//         },
+//         {
+//             path: '/post/:id',
+//             component: PostShow,
+//             name: 'post.show',
+//             props: true,
+//         },
+//
+//         {
+//             path: '/login',
+//             component: LoginPage,
+//             name: 'login'
+//         },
+//         {
+//             path: '/register',
+//             component: RegisterPage,
+//             name: 'register'
+//         }
+//     ],
+});
+
+export default router;
